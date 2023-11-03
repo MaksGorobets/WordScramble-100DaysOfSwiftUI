b390a0da0b620b66fe60c047963bd9061bcc277d
MaksGorobets <gorobets542@gmail.com>
2023-11-03 18:29:02 +0200

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Maks Winters on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
